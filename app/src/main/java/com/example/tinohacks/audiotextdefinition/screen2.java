5bc6caece52a5d4965f52bb7bff14c8b57354bf2
TinoHacks <akash.balakrish@gmail.com>
2017-04-16 01:12:29 -0700

    Life

--- app/src/main/java/com/example/tinohacks/audiotextdefinition/screen2.java
+++ app/src/main/java/com/example/tinohacks/audiotextdefinition/screen2.java
@@ -3,11 +3,38 @@ package com.example.tinohacks.audiotextdefinition;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+
 public class screen2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen2);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(screen2.this, android.R.layout.simple_list_item_1);
+
+
+//        adapter.add(word + ":" + def);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        listView.setAdapter(adapter);
+
+//        ListView Speech = (ListView) findViewById(R.id.listView);
+//        adapter.add(sentence);
     }
+
 }
+
