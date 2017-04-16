1a4a0ba504ba2dd9b2d90303f6901a8d47d0bda4
Pkfire26 <pulkitsi@gmail.com>
2017-04-16 06:05:50 -0700

    new update

--- app/src/main/java/com/example/tinohacks/audiotextdefinition/MainActivity.java
+++ app/src/main/java/com/example/tinohacks/audiotextdefinition/MainActivity.java
@@ -1,13 +1,85 @@
 package com.example.tinohacks.audiotextdefinition;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
+    public static ArrayList<String> myArray = new ArrayList<String>();
+    private final int SPEECH_RECOGNITION_CODE = 1;
+    private TextView txtOutput;
+    Button record;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        record = (Button) findViewById(R.id.rec);
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startSpeechToText();
+
+            }
+        });
+        myArray = new ArrayList<String>();
     }
+
+    private void startSpeechToText() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                "Speak something...");
+        try {
+            startActivityForResult(intent, SPEECH_RECOGNITION_CODE);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry! Speech recognition is not supported in this device.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case SPEECH_RECOGNITION_CODE: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String text = result.get(0);
+
+                    StringTokenizer st = new StringTokenizer(text);
+
+
+                    while (st.hasMoreElements()) {
+                        myArray.add(st.nextToken());
+                    }
+
+
+                }
+                break;
+
+            }
+
+
+
+        }
+        Intent inter = new Intent(getApplication(), screen2.class);
+        startActivity(inter);
+    }
+
 }
--- app/src/main/java/com/example/tinohacks/audiotextdefinition/screen2.java
+++ app/src/main/java/com/example/tinohacks/audiotextdefinition/screen2.java
@@ -1,40 +1,57 @@
 package com.example.tinohacks.audiotextdefinition;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.StringTokenizer;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class screen2 extends AppCompatActivity {
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen2);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(screen2.this, android.R.layout.simple_list_item_1);
 
-
-//        adapter.add(word + ":" + def);
-
+//      adapter.add(word + ":" + def);
         ListView listView = (ListView) findViewById(R.id.listView);
 
-        listView.setAdapter(adapter);
+
 
 //        ListView Speech = (ListView) findViewById(R.id.listView);
 //        adapter.add(sentence);
+        // int size = adapter.getCount();
+
+        for(int i = 0; i < MainActivity.myArray.size(); i ++){
+            adapter.add(MainActivity.myArray.get(i) + ":"+ "definition");
+        }
     }
 
 }
+
 
